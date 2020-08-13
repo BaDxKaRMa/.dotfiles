bbf1c1c6dc1dd6bde8cb0a1b586922b87bb614ae
BaDxKaRMa <bad87karma@gmail.com>
2020-08-13 08:06:21 -0400

    fixed volume bar

--- .config/awesome/components/volume-adjust.lua
+++ .config/awesome/components/volume-adjust.lua
@@ -81,9 +81,9 @@ awesome.connect_signal("volume_change",
    function()
       -- set new volume value
       awful.spawn.easy_async_with_shell(
-         "amixer sget Master | grep 'Mono:' | awk -F '[][]' '{print $2}'| sed 's/[^0-9]//g'",
+         "amixer sget Master | grep 'Left:' | awk -F '[][]' '{print $2}'| sed 's/[^0-9]//g'",
          function(stdout)
-            volume_bar.value = stdout
+            volume_bar.value = tonumber(stdout)
          end,
          false
       )
