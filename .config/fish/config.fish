df59df1cda56fcc5f7481a8599ea2fd4784cd4c5
Ralph Hittell <ralph.hittell@grainger.com>
2020-09-01 09:08:58 -0400

    htop alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -28,6 +28,7 @@ alias l='ls -l'
 alias la='ls -a'
 alias lla='ls -la'
 alias lt='ls --tree'
+alias htop='sudo htop'
 
 function fssh -d "Fuzzy-find ssh host via ag and ssh into it"
   ag --ignore-case '^host [^*]' ~/.ssh/config | cut -d ' ' -f 2 | fzf | read -l result; and ssh "$result"
