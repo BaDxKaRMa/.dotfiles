6852e12e2e2b2afcc88831489e8d0bb6ab7ca9bc
Ralph Hittell <ralph.hittell@grainger.com>
2020-12-22 15:41:22 -0500

    removed space

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,7 +25,7 @@ alias la='ls -a'
 alias lla='ls -la'
 alias lt='ls --tree'
 alias htop='sudo htop'
-alias keka='open -a Keka '
+alias keka='open -a Keka'
 
 function fssh -d "Fuzzy-find ssh host via ag and ssh into it"
   ag --ignore-case '^host [^*]' ~/.ssh/config | cut -d ' ' -f 2 | fzf | read -l result; and ssh "$result"
