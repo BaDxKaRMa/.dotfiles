e59e96d563246778c36c04f7ba9cbd47a67a2a35
Ralph Hittell <ralph.hittell@grainger.com>
2021-02-22 11:08:15 -0500

    added M1 and Intel support to fzf.zsh

--- .config/zsh/fzf.zsh
+++ .config/zsh/fzf.zsh
@@ -1,13 +1,23 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+
+# Check for M1
+if [ -d "/opt/homebrew/" ] 
+then
+	if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+		export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+	fi
+	[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+	source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+# Check for Intel
+if [ -d "/usr/local/Homebrew/" ] 
+then
+	if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+		export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+	fi
+	[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+	source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+fi
 
-# Key bindings
-# ------------
-source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
