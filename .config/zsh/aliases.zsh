44ba75416371c6819a879f4763da4ca6f274f38b
BaDxKaRMa <bad87karma@gmail.com>
2021-02-24 20:25:50 -0500

    new aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,17 +1,30 @@
 alias cls="clear"
 alias sshhosts="grep -w -i -E 'Host|HostName' ~/.ssh/config | sed 's/Host //' | sed 's/HostName //'"
+
 alias slog="script -a ~/logs/Terminal_Logging/(date +"%Y_%m_%d_%I_%M_%p").log"
 alias play="asciinema play ~/logs/(ls ~/logs/ | fzf -q '.cast ')"
+
 alias grep="grep -i"
+
 alias dot='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias dcam='dot commit -a -m'
+alias dst='dot status'
+alias dl='dot pull'
+alias dp='dot push'
+
 alias ls='lsd --group-dirs first'
 alias l='ls -l'
 alias la='ls -a'
 alias lla='ls -la'
 alias lt='ls --tree'
+
 alias htop='sudo htop'
+
 alias keka='open -a Keka'
+
 alias brewlist="brew leaves | xargs brew deps --include-build --tree"
+
 alias checkcert='CheckCert.sh -c'
+
 alias vi='vim'
-alias dcam='dot commit -a -m'
+
