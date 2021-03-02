e2d562774d5dd8831835c88b37e4562283407093
BaDxKaRMa <bad87karma@gmail.com>
2021-03-01 21:42:15 -0500

    added fssh

--- .config/zsh/environment.zsh
+++ .config/zsh/environment.zsh
@@ -3,9 +3,9 @@ export EDITOR='vim'
 
 # FZF
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --no-ignore'
+export FZF_DEFAULT_OPTS="--ansi --multi --layout=reverse --bind ctrl-a:select-all+accept --preview-window 'right:60%' --preview 'bat --color=always --style=header,grid --line-range :300 {}'"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_R_OPTS='-e --cycle --height 76% --border --layout=reverse'
-export FZF_DEFAULT_OPTS="--ansi --multi --layout=reverse --bind ctrl-a:select-all+accept --preview-window 'right:60%' --preview 'bat --color=always --style=header,grid --line-range :300 {}'"
 export FZF_ALT_C_COMMAND='fd --type d --hidden --follow --no-ignore'
 export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -100'"
 bindkey "ç" fzf-cd-widget
